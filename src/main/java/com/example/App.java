d4ab11bc13314c59db3a53064846b3f1ada1e22f
Ni-coco <nicolas.garde@outlook.com>
2022-12-09 18:29:45 +0100

    a

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
         new frame();
     }
 }
--- src/main/java/com/example/frame.java
+++ src/main/java/com/example/frame.java
@@ -11,6 +11,9 @@ import java.awt.Dimension;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 import java.awt.event.*;
 
 import javax.swing.*;
@@ -19,15 +22,16 @@ import java.util.*;
 public class frame extends JFrame implements ActionListener {
 
     List<String> crypto = getCrypto();
-    ImageIcon btc = new ImageIcon(new ImageIcon(getClass().getClassLoader().getResource("img/btc.png")).getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
-    ImageIcon eth = new ImageIcon(new ImageIcon(getClass().getClassLoader().getResource("img/eth.png")).getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
+    List<Double> next = getNext();
+    List<ImageIcon> img = getImg();
     JFrame win = new JFrame("Crypto prices");
-    JPanel[] pn = new JPanel[2];
-    JLabel[] icon = new JLabel[10];
-    JLabel[] price = new JLabel[10];
+    JPanel[] pn = new JPanel[4];
+    JLabel[] icon = new JLabel[6];
+    JLabel[] price = new JLabel[6];
     JButton refresh = new JButton("Refresh");
 
     public frame() {
+
         /* Set frame */
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         win.setPreferredSize(new Dimension(1080, 720));
@@ -35,49 +39,100 @@ public class frame extends JFrame implements ActionListener {
         win.setLayout(new BorderLayout());
         refresh.addActionListener(this);
 
-        /* set Panel */
-        pn[0] = new JPanel();
-        pn[0].setBackground(Color.DARK_GRAY);
-        pn[0].setLayout(new GridLayout(0, 2));
+        /* set Panels */
+        for (int i = 0; i < pn.length; i++)
+            pn[i] = new JPanel();
+        pn[0].setLayout(new FlowLayout());
+        pn[0].setBackground(Color.WHITE);
+        pn[1].setLayout(new BorderLayout());
+        pn[2].setBackground(Color.WHITE);
+        pn[2].setLayout(new GridLayout(0, 1, 20, 35));
+        pn[3].setBackground(Color.BLACK);
+        pn[3].setLayout(new GridBagLayout());
 
-        /* Icon of Cryptocurrency */
-        icon[0] = new JLabel();
-        icon[0].setIcon(btc);
-        icon[1] = new JLabel();
-        icon[1].setIcon(eth);
 
-        /* Prices for Cryptocurrecy */
-        for (int i = 0; i < 2; i++) {
+        /* Icon and Prices of Cryptocurrency */
+        for (int i = 0; i < crypto.size(); i++) {
+            icon[i] = new JLabel();
+            icon[i].setIcon(img.get(i));
             price[i] = new JLabel();
             price[i].setForeground(Color.GREEN);
         }
 
         /* Adding Icon and Price to panel */
-        pn[0].add(icon[0]);
-        pn[0].add(price[0]);
-        pn[0].add(icon[1]);
-        pn[0].add(price[1]);
         pn[0].add(refresh);
+        GridBagConstraints c = new GridBagConstraints();
+        for (int i = 0; i < crypto.size(); i++) {
+            pn[2].add(icon[i]);
+            c.gridy = i;
+            pn[3].add(price[i], c);
+        }
 
         /* Adding panel to frame */
-        win.add(pn[0]);
+        win.add(pn[0], BorderLayout.NORTH);
+        win.add(pn[1], BorderLayout.CENTER);
+        pn[1].add(pn[2], BorderLayout.WEST);
+        pn[1].add(pn[3], BorderLayout.CENTER);
 
         /* Visibility and pack for frame */
-        win.setVisible(true);
         win.pack();
+        win.setVisible(true);
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < crypto.size(); i++)
+            price[i].setText(next.get(i).toString());
+
+        /*for (;;) {
+            for (int i = 0; i < crypto.size(); i++) {
+                try {
+                    next.set(i, Double.parseDouble(getPrices(crypto.get(i))));
+                    if (next.get(i) < Double.parseDouble(price[i].getText()))
+                        price[i].setForeground(Color.RED);
+                    else
+                        price[i].setForeground(Color.GREEN);
+                    price[i].setText(next.get(i).toString());
+                    if (i == crypto.size() - 1)
+                        Thread.sleep(10000);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+        }*/
+    }
+
+
+    public List<String> getCrypto() {
+        List<String> list = new ArrayList<String>();
+        list.add("BTC");
+        list.add("ETH");
+        list.add("BNB");
+        list.add("SOL");
+        list.add("FTM");
+        list.add("WLKN");
+        return list;
+    }
+    
+
+    public List<Double> getNext() {
+        List<Double> list = new ArrayList<Double>();
+        for (int i = 0; i < crypto.size(); i++) {
             try {
-                price[i].setText(getPrices(crypto.get(i)));
+                list.add(Double.parseDouble(getPrices(crypto.get(i))));
             } catch (Exception e) {
-                System.exit(1);
+                System.out.println(e);
             }
         }
+        return list;
+    }
+
+    public List<ImageIcon> getImg() {
+        List<ImageIcon> list = new ArrayList<ImageIcon>();
+        for (int i = 0; i< crypto.size(); i++)
+            list.add(new ImageIcon(new ImageIcon(getClass().getClassLoader().getResource("img/"+crypto.get(i)+".png")).getImage().getScaledInstance(70, 70, Image.SCALE_DEFAULT)));
+        return list;
     }
 
     public String getPrices(String str) throws Exception {
-        String url = "https://min-api.cryptocompare.com/data/price?fsym="+str+"&tsyms=USD,EUR,CNY,JPY,GBP";
-        URL financeUrl = new URL(url);
+        URL financeUrl = new URL("https://min-api.cryptocompare.com/data/price?fsym="+str+"&tsyms=USD&api_key=ef6c349317448a25c217cdab8e57e2c94bb8e053f17c51ca600efcf2ae862a1b");
         InputStreamReader reader = new InputStreamReader(financeUrl.openStream());
         JsonReader jsonReader = new JsonReader(reader);
         JsonParser parser = new JsonParser();
@@ -85,21 +140,18 @@ public class frame extends JFrame implements ActionListener {
         return response.get("USD").getAsString();
     }
 
-    public List<String> getCrypto() {
-        List<String> list = new ArrayList<String>();
-        list.add("BTC");
-        list.add("ETH");
-        return list;
-    }
-
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == refresh) {
-            for (int i = 0; i < 2; i++) {
+            for (int i = 0; i < crypto.size(); i++) {
                 try {
-                    System.out.println(getPrices(crypto.get(i)));
-                    price[i].setText(getPrices(crypto.get(i)));
+                    next.set(i, Double.parseDouble(getPrices(crypto.get(i))));
+                    if (next.get(i) < Double.parseDouble(price[i].getText()))
+                        price[i].setForeground(Color.RED);
+                    else
+                        price[i].setForeground(Color.GREEN);
+                    price[i].setText(next.get(i).toString());
                 } catch (Exception a) {
-                    System.exit(1);
+                    System.out.println(a);
                 }
             }
         }
